b63673792d9218c10e25d1b401789052eac72ae9
Jhorie Ranario <jmranario@student.apc.edu.ph>
2026-02-22 13:57:51 +0000

    simplify CORS configuration and remove unused Vercel settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,13 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-     origin: [
-      'https://personal-website-finals-qnk1-bcc4z2rv5-jhories-projects.vercel.app',
-      'http://localhost:5173',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    origin: '*',
   });
   
   app.useGlobalPipes(new ValidationPipe());
